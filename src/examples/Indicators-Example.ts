30c997d04ad1bec545004821ab96d029dcc81149
mikolyk <amikolyk@yandex.ru>
2025-10-08 14:12:12 +0300

    better example

--- src/examples/Indicators-Example.ts
+++ src/examples/Indicators-Example.ts
@@ -2,9 +2,11 @@ import { StandardReportLayout } from '../lib/report/layouts/standart.report.layo
 import { BaseScript } from '../lib/script/base-script';
 import { globals } from '../lib/core/globals';
 import { SimpleMovingAverageIndicator } from '../lib/indicators';
-import { trace } from '../lib/core/log';
+import { log, trace } from '../lib/core/log';
 import { AverageTrueRange } from '../lib/indicators/atr';
 import { CandlesBuffer } from '../lib/candles';
+import { BaseIndicator } from '../lib/indicators/base-indicator';
+import { timeCurrent } from '../lib/utils/date-time';
 
 /*
 Indicators Example
@@ -31,15 +33,7 @@ class Script extends BaseScript {
       key: 'symbols',
       defaultValue: 'XRP/USDT:USDT',
     },
-    {
-      key: 'sizeUsd',
-      defaultValue: 5,
-    },
   ];
-
-  // Strategy configuration
-  hedgeMode = true; // Enable hedge mode for futures trading
-  sizeUsd: number; // Position size in USD
 
   // Technical indicators
   private sma14: SimpleMovingAverageIndicator; // 200-period SMA
@@ -48,6 +42,7 @@ class Script extends BaseScript {
   // Report and data components
   private reportLayout: StandardReportLayout;
   private buffer: CandlesBuffer; // Candles buffer for historical data
+  private nextTime = 0;
 
   constructor(params: GlobalARGS) {
     super(params);
@@ -55,14 +50,23 @@ class Script extends BaseScript {
 
   async onInit() {
     // Initialize standard report layout
-    this.reportLayout = new StandardReportLayout();
+    this.reportLayout = new StandardReportLayout({
+      title: 'Indicators Example',
+      description: `Exchange: ${this.connectionName}, marketType: ${this.marketType} Symbol: ${this.symbols[0]}`,
+    });
 
     // Create SMA indicator: 25-period on 1m timeframe
     this.sma14 = await globals.indicators.sma(this.symbols[0], '1m', 25);
     globals.report.tableUpdate('sma14 onInit', this.sma14.getIndicatorValues());
+    this.showChart('SMA', this.sma14, true);
 
     // Create ATR indicator: 14-period on 1m timeframe
     this.art14 = await globals.indicators.atr(this.symbols[0], '1m', 14);
+    this.showChart('ATR', this.art14);
+
+    //natr
+    const natr14 = await globals.indicators.natr(this.symbols[0], '1m', 14);
+    this.showChart('NATR', natr14);
 
     // Get candles buffer for historical data access
     this.buffer = await globals.candlesBufferService.getBuffer({
@@ -74,20 +78,36 @@ class Script extends BaseScript {
     trace('Script:onInit', 'Indicator info', this.sma14.getInfo());
   }
 
-  /**
-   * Called on every price tick
-   * Updates charts with current indicator values
-   */
+  showChart(chartName, indicator: BaseIndicator, isAddPrice = false) {
+    const len = indicator.length;
+    trace('Script:showChart', `Adding ${len} points to chart ${chartName}`, {}, true);
+    let candleBuff = indicator.getCandlesBuffer();
+    for (let i = len - 1; i >= 0; i--) {
+      //
+      const val = indicator.getValue(i);
+      const time = indicator.getTimestamp(i);
+      const name = indicator.constructor?.name ?? 'Indicator';
+      globals.report.chartAddPointXY(chartName, name, time, val);
+      if (isAddPrice) {
+        globals.report.chartAddPointXY(chartName, 'Price', time, candleBuff.close(i));
+        // globals.report.chartAddPointXY(chartName + 0, 'Diff', time, candles[i].timestamp - time);
+      }
+    }
+  }
+
   iterator = 0;
   async onTick(): Promise<void> {
-    // Add SMA value to chart
-    globals.report.chartAddPoint('SMA', 'sma14', this.sma14.getValue());
+    if (this.nextTime < timeCurrent()) {
+      // Add SMA value to chart
+      globals.report.chartAddPoint('SMA', 'sma14', this.sma14.getValue());
 
-    // Add current price to chart
-    globals.report.chartAddPoint('SMA', 'price', close());
+      // // Add current price to chart
+      // globals.report.chartAddPoint('SMA', 'price', close());
 
-    // Add ATR value to chart
-    globals.report.chartAddPoint('ATR', 'atr14', this.art14.getValue());
+      // Add ATR value to chart
+      globals.report.chartAddPoint('ATR', 'atr14', this.art14.getValue());
+      this.nextTime = timeCurrent() + 60 * 1000;
+    }
   }
 
   /**
@@ -95,11 +115,10 @@ class Script extends BaseScript {
    * Displays historical indicator data in tables
    */
   async onStop(): Promise<void> {
-    await super.onStop();
-
     // Display ATR indicator values in table
     // (Candles buffer table is commented out to avoid clutter)
     // globals.report.tableUpdate('CandlesBuffer onStop', this.buffer.getCandles());
     globals.report.tableUpdate('atr14 onStop', this.art14.getIndicatorValues());
+    log('Script:onStop', 'Indicators Example', {}, true);
   }
 }
