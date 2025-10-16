9c4a93922f5660ddca4e71276c8b2f4f9607642e
Nikolay <bukharin17@gmail.com>
2025-10-16 15:06:27 +0400

    hotfix: indicators paths

--- src/lib/indicators/index.ts
+++ src/lib/indicators/index.ts
@@ -2,4 +2,4 @@ export { Indicators } from './indicators';
 export { SimpleMovingAverageIndicator } from './sma';
 export { RelativeStrengthIndex } from './rsi';
 export { CommodityChannelIndex } from './cci';
-export { NormalizedAverageTrueRange } from './natr';
+export { NormalizedAverageRangeTrue } from './nart';
